56532c1e390cb48142e1b759cb463a8b8cf8f212
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-08 15:39:29 +0300

    :zap: added option parser for video file path

--- src/main.py
+++ src/main.py
@@ -2,46 +2,58 @@
 
 import cv2
 
-# Load video files
-ped_video = cv2.VideoCapture('videos/ped.mp4')
-# Load cascade files
-car_cascade = 'haar_cascades/car.xml'
-ped_cascade = 'haar_cascades/body.xml'
+class Classifier:
 
-# create classifiers
-car_classifier = cv2.CascadeClassifier(car_cascade)
-ped_classifier = cv2.CascadeClassifier(ped_cascade)
+  def __init__(self, path):
+    # Load video files
+    self.ped_video = cv2.VideoCapture(path)
+    # Load cascade files
+    self.car_cascade = 'haar_cascades/car.xml'
+    self.ped_cascade = 'haar_cascades/body.xml'
+    # create classifiers
+    self.car_classifier = cv2.CascadeClassifier(self.car_cascade)
+    self.ped_classifier = cv2.CascadeClassifier(self.ped_cascade)
 
-# open video
-while ped_video.isOpened():
-  # start reading video frames
-  ret, frame = ped_video.read()
-  # if next frame grabbed
-  if ret:
-    # transform to gray
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # detect each car
-    cars = car_classifier.detectMultiScale(gray)
-    # detect each pedestrian
-    peds = ped_classifier.detectMultiScale(gray)
-    # place a rectangle around car
-    for (x, y, w, h) in cars:
-      cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-    # place a rectangle around pedestrians too
-    for (x, y, w, h) in peds:
-      cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    # show frame
-    cv2.imshow('DETECTION', frame)
-    # listen for keys
-    key_pressed = cv2.waitKey(1)
+  def detect(self):
+    # open video
+    while self.ped_video.isOpened():
+      # start reading video frames
+      ret, frame = self.ped_video.read()
+      # if next frame grabbed
+      if ret:
+        # transform to gray
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # detect each car
+        cars = self.car_classifier.detectMultiScale(gray)
+        # detect each pedestrian
+        peds = self.ped_classifier.detectMultiScale(gray)
+        # place a rectangle around car
+        for (x, y, w, h) in cars:
+          cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        # place a rectangle around pedestrians too
+        for (x, y, w, h) in peds:
+          cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # show frame
+        cv2.imshow('DETECTION', frame)
+        # listen for keys
+        key_pressed = cv2.waitKey(1)
 
-  if key_pressed == 81 or key_pressed == 113:
-    break 
+      if key_pressed == 81 or key_pressed == 113:
+        break 
 
-ped_video.release()
-cv2.destroyAllWindows()
+    self.ped_video.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-  from option
-  
+  from optparse import OptionParser
+  parser = OptionParser()
+  parser.add_option('--video', action='store', default='videos/ped.mp4', type='string',
+                    dest='video', help='path to the video file')
+
+  (options, args) = parser.parse_args()
+
+  if options.video:
+    video_file = options.video
+    classifier = Classifier(video_file)
+    classifier.detect()
